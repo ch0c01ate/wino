d2f832d2c2550dd8ab26e2487c1ca4f94e4cd7d0
Max <hrytsay@ucu.edu.ua>
2020-01-19 15:54:12 +0200

    still raw photo parser

--- photo_parser.py
+++ photo_parser.py
@@ -1,5 +1,24 @@
 from PIL import Image
 import numpy
+import math
+
+
+def to_dict(img):
+    res = dict()
+    for row in range(len(img)):
+        for col in range(len(img)[row]):
+            val = img[row][col]
+            if val not in res:
+                res[val] = (row, col)
+
+
+def get_med(img):
+    tmp = sorted(img.keys())
+    return tmp[len(tmp) // 2]
+
+
+def get_med_coords(img, med, accuracy=10):
+    pass
 
 
 if __name__ == '__main__':
